727ce994185e2a760eb4463cb0aa5f41d6e3c433
Sam <sscheinman+udacityJSBasics@gmail.com>
2016-05-25 22:32:14 -0400

    Clean resumeBuilder.js file.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-/*$("#main").append(["Sam<br><br>"]);*/
-
-var skills = ["Awesomeness", "Wood Burning", "Burning Chrome", "Mnemonic Couriering"];
 
 var bio = {
     "name" : "Sam",
     "age" : 36,
-    "skills": skills
+    "skills": ["Awesomeness", "Wood Burning", "Burning Chrome", "Mnemonic Couriering"]
 };
 
 var work = {
@@ -35,25 +32,12 @@ var education = {
     ]
 };
 
+var project{
+    
+};
+
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", "King of All That I Survey");
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-
-/*var awesomeThoughts="I am Sam, king of all that I survey.<br><br>";
-console.log(awesomeThoughts);
-var funThought = awesomeThoughts.replace("king", "bling");
-$("#main").append(funThought);*/
-
-$("#main").append("Biography:<br><br>");
-$("#main").append("Age: " + bio.age + "<br><br>");
-
-$("#main").append("Current Position: " + work["position"] + "<br><br>");
-
-$("#main").append("School: " + education.name + "<br><br>");
-
-
-var udacityStr = "audacity";
-udacityStr = "U" + udacityStr.slice(2);
-console.log(udacityStr);
